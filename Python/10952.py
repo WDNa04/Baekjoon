1186af5065a71624759842c90ffe33cfec1604a9
WDNa04 <WNa0531@outlook.com>
2023-06-23 00:16:00 +0900

    pass

--- Python/10952.py
+++
@@ -1,7 +0,0 @@
-import sys
-while True:
-    input = str(sys.stdin.readline())
-    output1,output2 = map(int,input.split())
-    if output1==output2==0:
-        break
-    print(output1+output2)
--- Python/2439.py
+++
@@ -1,4 +0,0 @@
-import sys
-input = int(sys.stdin.readline())
-for i in range(1, input + 1):
-    print(" " * (input - i) + "*" * i)
