a6501e5ad3548ec514f94d864cff2d17c9c60ae1
WDNa04 <137413210+WDNa04@users.noreply.github.com>
2023-06-29 21:15:11 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,13 @@
+import sys
+count = sys.stdin.readline()
+a, b = map(int,count.split())
+listc = []
+for i in range(2*a):
+    number = sys.stdin.readline()
+    lista = list(map(int,number.split()))
+    listc.append(lista)
+for i in range(a):
+    listd = []
+    for l in range(b):
+        listd.append(listc[i][l]+listc[i+a][l])
+    print(*listd)
