73ca964bd54fa9dc151aee0cf63bb11664fde58c
WDNa04 <WNa0531@outlook.com>
2023-06-29 23:28:49 +0900

    hi

--- test.py
+++ test.py
@@ -1,27 +1,16 @@
 import sys
-lista = [['c=', 'č'], ['c-','ć'],['dz=','dž'], ['d-', 'đ'], ['lj','lj'],['nj','nj'],['s=','š'], ['z=','ž']]
-name = sys.stdin.readline()
-name = name.strip()
-name2 = ''
-listb = []
-a = 0
-i = 0
-while i < len(name):
-    name2 += name[i]
-    for k in lista:
-        if k[0] in name2:
-            listb.append(k[0])
-            name2 = name2.replace(k[0],'')
-            a += len(name2)
-            name2 = ''
-        else:
-            pass
-    i+=1
-    if i == len(name):
-        a+=len(name2)
-
-a += len(listb)
-print(a)
-
+count = sys.stdin.readline()
+a, b = map(int,count.split())
+listc = []
+for i in range(2*a):
+    number = sys.stdin.readline()
+    lista = list(map(int,number.split()))
+    listc.append(lista)
+print(listc)
+for i in range(a):
+    listd = []
+    for l in range(b):
+        listd.append(listc[i][l]+listc[i+a][l])
+    print(*listd)
 
 
