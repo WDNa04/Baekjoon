763157b9df0d28f7b6392e635d8ab096f56255a9
WDNa04 <WNa0531@outlook.com>
2023-08-01 11:56:33 +0800

    hi

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
 import sys
 
-number = int(sys.stdin.readline())
-v = 1
-while True:
-    list = []
-    for i in range(1, v + 1):
-        if v % i == 0:
-            list.append(i)
-    if number % v == 0 and len(list) == 2:
-        number = int(number / v)
-        print(v)
-        if number == 1:
-            break
-    else:
-        v += 1
+size = int(sys.stdin.readline())
+MenOfPassion(A[],n) {
+    i = [n / 2];
+    return A[i];
+}
