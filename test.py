f7f017f49decf99abe0876f91c1b44fb3ab4e776
WDNa04 <WNa0531@outlook.com>
2023-09-11 20:30:30 +0800

    ih

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-import sys
+def binom_coeff(n,k):
+    a = n
+    multi_n = 1
+    while a>0:
+        multi_n *= a
+        a -= 1
+    b = n-k
+    multi_n_k = 1
+    while b > 0:
+        multi_n_k *= b
+        b -= 1
+    c = k
+    multi_k = 1
+    while c > 0:
+        multi_k *= c
+        c -= 1
+    return int(multi_n / multi_n_k / multi_k)
 
-def findKthLargest(nums, k):
-    nums_list = list(map(int,nums.split(',')))
-    nums_list.remove(max(nums_list))
-    return max(nums_list)
-
-nums = sys.stdin.readline()
-k = int(sys.stdin.readline())
-print(findKthLargest(nums, k))
-
-
+print(binom_coeff(int(input()),int(input())))
