1c3362fdf288b6733f35aa54b4538ed79760647a
WDNa04 <WNa0531@outlook.com>
2023-10-10 17:45:29 +0800

    hi

--- test.py
+++ test.py
@@ -1,19 +1,27 @@
-def binom_coeff(n,k):
-    a = n
-    multi_n = 1
-    while a>0:
-        multi_n *= a
-        a -= 1
-    b = n-k
-    multi_n_k = 1
-    while b > 0:
-        multi_n_k *= b
-        b -= 1
-    c = k
-    multi_k = 1
-    while c > 0:
-        multi_k *= c
-        c -= 1
-    return int(multi_n / multi_n_k / multi_k)
+def is_ancestor(name1,name2,pdict):
+    while name2 != name1:
+        if pdict.get(name2) != None:
+            name2 = pdict[name2]
+        else:
+            return False
+    return True
 
-print(binom_coeff(int(input()),int(input())))
+def is_related(name1,name2,pdict):
+    list1 = [name1]
+    while pdict.get(name1) != None:
+        list1.append(pdict.get(name1))
+        name1 = pdict.get(name1)
+    list2 = [name2]
+    while pdict.get(name2) != None:
+        list1.append(pdict.get(name2))
+        name1 = pdict.get(name2)
+    for i in list1:
+        if i in list2:
+            return True
+    return False
+
+parent = {'Amy':'Ben', 'May':'Tom', 'Tom':'Ben',
+ 'Ben':'Howard', 'Howard':'George', 'Frank':'Amy',
+ 'Joe':'Bill', 'Bill':'Mary', 'Mary':'Philip', 'Simon':'Bill',
+ 'Zoe':'Mary'}
+print(is_ancestor('Mary','Simon',parent))
